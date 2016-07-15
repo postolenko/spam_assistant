30382936cb5a36615870ab28d749c8aadec24688
postolenko <denis.postolenko@yandex.ua>
2016-07-15 21:16:27 +0300

    Spam Assistant

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,8 @@ $(document).ready(function() {
     bodyHeight = w.innerHeight || e.clientHeight || g.clientHeight;
 
 
+    getNavigationHeight();
+
     getScrollToTopBtn();
 
 
@@ -19,14 +21,18 @@ $(document).ready(function() {
 
     $(window).resize(function() {
 
-
+        // getNavigationHeight();
 
     });
+
+    var navBlock;
+    var parentNavBlock;
+    var heightParentNavBlock;
 
 
     // Height Nav-side  = Height Content
 
-    // $(".nav-section").height( $(".main-content").height() - 70);
+    // $(".nav-section-bg").outerHeight( $(".main-content").height() - 70);
     // console.log($(".content").height());
 
     // Show or hide ".scroll-to-top" button
@@ -37,18 +43,42 @@ $(document).ready(function() {
     var countSections = 0;
     // var countBorders = 0;
 
-    for(countSections = 0; countSections <= ( $(".content-section").length - 1); countSections++ ) {
+    // for(countSections = 0; countSections <= ( $(".content-section").length - 1); countSections++ ) {
 
-        $(".border:eq("+ countSections +")").css({"height" : $(".content-section:eq("+ countSections +")").outerHeight(true) + "px"});
+    //     // $(".border:eq("+ countSections +")").css({"height" : $(".content-section:eq("+ countSections +")").outerHeight(true) + "px"});
 
-        if ( $(".border:eq("+ countSections +")").hasClass("none") ) {
+    //     // if ( $(".border:eq("+ countSections +")").hasClass("none") ) {
             
-            $(".border:eq("+ countSections +")").css({"height" : 0 + "px"});
+    //     //     $(".border:eq("+ countSections +")").css({"height" : 0 + "px"});
 
-        }
+    //     // }
+
+    //     console.log( $(".border:eq("+ countSections +")").offset().top );
+
+    // }
+
+
+    // $(".border.first-coor").height( $(".last-coor").offset().top - $(".first-coor").offset().top );
+
+
+
+// -----------------
+
+    function getNavigationHeight() {
+
+        // navBlock = document.getElementsByClassName("nav-section-bg")[0];
+        // parentNavBlock = document.getElementsByClassName("nav-section")[0];
+
+        // heightParentNavBlock = parentNavBlock.innerHeight || parentNavBlock.clientHeight || parentNavBlock.clientHeight;
+
+        // navBlock.style.height = heightParentNavBlock - 70 + "px";
+
+        // console.log(heightParentNavBlock);
 
     }
 
+
+// -----------------
 
     $(".scroll-to-top").click(function () {
 
